e69ca472f40eff799cf1264cb1efe7bb91976ae7
Emmanuel Victor Garcia <emmanuel.garcia@xurpas.com>
2018-09-16 12:25:02 +0800

    Added UserActivity

--- app/src/main/java/io/github/regres/ui/user/UserActivity.kt
+++ app/src/main/java/io/github/regres/ui/user/UserActivity.kt
@@ -1,14 +1,20 @@
 package io.github.regres.ui.user
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.gson.Gson
 import io.github.regres.R
+import io.github.regres.data.entities.User
+import io.github.regres.databinding.ActivityUserBinding
 import io.github.regres.ui.base.BaseActivity
 
 class UserActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user)
     }
 
     override fun configureViewModel() {
@@ -16,7 +22,10 @@ class UserActivity : BaseActivity() {
     }
 
     override fun configureUI() {
-        
+        val data = Gson().fromJson(intent.getStringExtra("USER_INTENT"), User::class.java)
+        binding.apply {
+            user = data
+        }
     }
 
 }
